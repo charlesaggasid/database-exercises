6c0c90a9cb38147991a8c23d990262c32493f097
Charles Aggasid <charles.aggasid01@gmail.com>
2022-01-27 11:26:54 -0600

    limit exercise

---
+++ limit_exercises.sql
@@ -0,0 +1,18 @@
+USE employees;
+
+SELECT DISTINCT last_name FROM employees
+ORDER BY last_name DESC
+LIMIT 10;
+
+SELECT emp_no from salaries
+ORDER BY salary DESC
+LIMIT 5;
+
+SELECT emp_no
+FROM salaries
+ORDER BY salary DESC
+LIMIT 4 OFFSET 46;
+
+
+show tables;
+select * from salaries;
