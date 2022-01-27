f3bc3fbabc361138a9c872f43f67228a1ebd8dea
Charles Aggasid <charles.aggasid01@gmail.com>
2022-01-27 09:29:03 -0600

    where exercise done

--- where_exercises.sql
+++ where_exercises.sql
@@ -8,8 +8,10 @@ SELECT * FROM employees WHERE last_name LIKE 'E%';
 
 SELECT * FROM employees WHERE last_name LIKE '%q%';
 
-SELECT * FROM employees WHERE first_name = 'Irena' or first_name = 'Vidya' or first_name = 'Maya' or gender = 'm';
+SELECT * FROM employees WHERE gender = 'm' AND (first_name = 'Irena' or first_name = 'Vidya' or first_name = 'Maya');
 SELECT * FROM employees WHERE last_name LIKE 'E%' or last_name LIKE '%E';
 SELECT * FROM employees WHERE last_name LIKE 'E%' AND last_name LIKE '%E';
 
 SELECT * FROM employees WHERE last_name LIKE '%q%'  AND last_name NOT LIKE '%qu%';
+
+SELECT first_name AS 'First Name lists', last_name AS 'Last Name' from employees where gender = 'm';
