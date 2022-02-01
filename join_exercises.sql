6ff838be98f82a41ebba1d092d7af74a89c2ff93
Charles Aggasid <charles.aggasid01@gmail.com>
2022-02-01 09:04:15 -0600

    join exercise finish

--- join_exercises.sql
+++ join_exercises.sql
@@ -58,10 +58,8 @@ ORDER BY dept_name;
 # Find the current titles of employees currently working in the Customer Service department.
 SELECT t.title as 'Titles', COUNT(t.emp_no) as 'Total'
 FROM titles AS t
-    JOIN dept_emp AS de
-        ON de.emp_no = t.emp_no
-    JOIN departments as d
-        ON de.dept_no = d.dept_no
+JOIN dept_emp AS de ON de.emp_no = t.emp_no
+JOIN departments AS d ON de.dept_no = d.dept_no
 WHERE de.to_date > NOW()
 AND d.dept_name = 'Customer Service'
 AND t.to_date > NOW()
@@ -70,7 +68,7 @@ GROUP BY title;
 # Find the current salary of all current managers
 SELECT d.dept_name AS 'Department Name', concat(e.first_name, ' ', e.last_name) AS 'Department Manager', s.salary AS Salary
 FROM employees AS e
-         JOIN dept_manager AS dm
+         JOIN dept_manager  AS dm
               ON dm.emp_no = e.emp_no
          JOIN departments AS d
               ON d.dept_no = dm.dept_no
@@ -79,6 +77,7 @@ FROM employees AS e
 WHERE dm.to_date > now()
   AND s.to_date > now()
 ORDER BY dept_name;
+
 
 
 
